b7f9f04fb37158013810f74a2fd361afbf4cdc7f
Abhishek Kushmesh <abhishek.2201225cs@iiitbh.ac.in>
2025-09-16 17:06:03 +0530

    latest footer

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import AddCourse from './pages/educator/AddCourse'
 import Mycourses from './pages/educator/Mycourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
+import "quill/dist/quill.snow.css";
 
 const App = () => {
 
